a21be579097cb257a1f1e01aa9c567065a8d1ab6
Sub2mval <62394699+Sub2mval@users.noreply.github.com>
2022-03-08 21:36:01 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 Byfunction validateform(){  
-    var name=document.myform.Name.value;  
-    var password=document.myform.Password.value;  
+    var name=document.myform.username.value;  
+    var password=document.myform.password.value;  
       
     if (name==null || name==""){  
       alert("Name can't be blank");  
